d3ba5ee71a5825dcdd9f283cb534382371115b8b
Jakub Pustelnik <jhirtiganto@gmail.com>
2017-02-25 18:34:51 +0100

    Ported Exercise_6_07_FlowField3DNois

---
+++ Exercise_6_07_FlowField3DNois/Exercise_6_07_FlowField3DNois.pyde
@@ -0,0 +1,56 @@
+# The Nature of Code
+# Daniel Shiffman
+# http://natureofcode.com
+
+# Ported by: Jakub Pustelnik
+
+# Flow Field Following
+# Via Reynolds: http://www.red3d.com/cwr/steer/FlowFollow.html
+from flowfield import FlowField
+from vehicle import Vehicle
+
+def setup():
+    size(640, 360)
+
+    global debug
+    debug = True
+
+    # Make a new flow field with "resolution" of 20
+    global flowfield
+    flowfield = FlowField(20)
+
+    # Make a whole bunch of vehicles with random maxspeed and maxforce values
+    global vehicles
+    vehicles = []
+    for _ in range(0, 120):
+        l = PVector(random(0, width), random(0, height))
+        ms = random(2, 5)
+        mf = random(0.1, 0.5)
+        vehicles.append(Vehicle(l, ms, mf))
+
+def draw():
+    background(255)
+    flowfield.update()
+
+    # Display the flowfield in "debug" mode
+    if debug:
+        flowfield.display()
+
+    # Tell all the vehicles to follow the flow field
+    for v in vehicles:
+        v.follow(flowfield)
+        v.run()
+
+    # Instructions
+    fill(0)
+    text("Hit space bar to toggle debugging lines.\n" +
+         "Click the mouse to generate a new flow field. " + str(frameRate) , 10, height - 20)
+
+def keyPressed():
+    global debug
+
+    if key == ' ':
+        debug = not debug
+
+def mousePressed():
+    flowfield.initialize()
---
+++ Exercise_6_07_FlowField3DNois/flowfield.py
@@ -0,0 +1,88 @@
+# The Nature of Code
+# Daniel Shiffman
+# http://natureofcode.com
+
+# Ported by: Jakub Pustelnik
+
+# Flow Field Following
+
+class FlowField:
+
+    def __init__(self, r):
+        self.resolution = r   # How large is each "cell" of the flow field
+        self.zoff = 0.0   # 3rd dimension of noise
+
+        # Determine the number of columns and rows based on sketch's width and
+        # height
+        self.cols = int(width / self.resolution)
+        self.rows = int(height / self.resolution)
+
+        self.initialize()
+
+    def initialize(self):
+        # Reseed noise so we get a new flow field every time
+        noiseSeed(int(random(10000)))
+
+        self.field = []
+
+        xoff = 0
+        for _ in range(0, self.cols):
+            yoff = 0
+            y = []
+            for _ in range(0, self.rows):
+                theta = map(noise(xoff, yoff), 0, 1, 0, TWO_PI)
+
+                y.append(PVector(cos(theta), sin(theta)))
+                yoff += 0.1
+            self.field.append(y)
+            xoff += 0.1
+
+    def update(self):
+        xoff = 0
+        for x in range(0, self.cols):
+            yoff = 0
+            y = []
+            for y in range(0, self.rows):
+                theta = map(noise(xoff, yoff, self.zoff), 0, 1, 0, TWO_PI)
+
+                self.field[x][y] = PVector.fromAngle(theta)
+                yoff += 0.1
+            xoff += 0.1
+        # Animate by changing 3rd dimension of noise every frame
+        self.zoff += 0.01
+
+    # Draw every vector
+    def display(self):
+        for x in range(0, self.cols):
+            for y in range(0, self.rows):
+                xres = x * self.resolution
+                yres = y * self.resolution
+                self.drawVector(
+                    self.field[x][y], xres, yres, self.resolution - 2)
+
+    # Renders a vector object 'v' as an arrow and a position 'x,y'
+    def drawVector(self, v, x, y, scayl):
+        pushMatrix()
+
+        # Translate to position to render vector
+        translate(x, y)
+        stroke(0, 100)
+
+        # Call vector heading function to get direction (note that pointing to
+        # the right is a heading of 0) and rotate
+        rotate(v.heading())
+
+        # Calculate length of vector & scale it to be bigger or smaller if
+        # necessary
+        leng = v.mag() * scayl
+
+        # Draw three lines to make an arrow (draw pointing up since we've
+        # rotate to the proper direction)
+        line(0, 0, leng, 0)
+
+        popMatrix()
+
+    def lookup(self, lookup):
+        column = int(constrain(lookup.x / self.resolution, 0, self.cols - 1))
+        row = int(constrain(lookup.y / self.resolution, 0, self.rows - 1))
+        return self.field[column][row].copy()
